e9d70b88948f2ccde5b383946f355701fdad400d
TrevorPeters <TrevorPeters@users.noreply.github.com>
2020-04-23 15:45:36 -0700

    This is my commit message

---
+++ app/src/main/java/edu/tacoma/uw/itsdone/FindJobsActivity.java
@@ -0,0 +1,24 @@
+package edu.tacoma.uw.itsdone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+//TODO all of this class tbh
+public class FindJobsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_jobs);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String username = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(username);
+    }
+}
