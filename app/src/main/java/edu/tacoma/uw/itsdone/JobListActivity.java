510da059bb4dd5858bd52e7fd329e56636ca2b2e
Gehry Guest <35783062+GuestG@users.noreply.github.com>
2020-05-13 14:37:48 -0700

    edited button bar, created simple layout for addjob class

--- app/src/main/java/edu/tacoma/uw/itsdone/JobListActivity.java
+++ app/src/main/java/edu/tacoma/uw/itsdone/JobListActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
